2727f17b8283e5e6adc6474dad92a79902488e29
Sakit <memmedlisakit@gmail.com>
2018-11-20 22:41:44 +0400

    first lecture

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { mul, div, sub, add } from "./math";
+// const { mul, div, sub, add } = require('./math');
+
+console.log(`multiple: ${mul(10, 3)}`);
+console.log(`divide: ${div(10, 3)}`);
+console.log(`addition: ${add(10, 3)}`);
+console.log(`subtract: ${sub(10, 3)}`);
---
+++ src/math.js
@@ -0,0 +1,10 @@
+const mul = (a, b) => a * b;
+const div = (a, b) => a / b;
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+
+export { mul, div, add, sub };
+
+// module.exports = {
+//     mul, div, add, sub
+// }
